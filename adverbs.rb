974829a4ac174d6e2eb4697628b9b118f3ed6e99
wfjeff <wm.jeffries@gmail.com>
2014-04-25 17:50:06 -0400

    first commit

---
+++ adverbs.rb
@@ -0,0 +1,32 @@
+distances_to_stars = [
+  1.234687121234088e+17,
+  1.029348710293847e+17,
+  2.3475986283475296e+16,
+  2.312341425092346e+19,
+  1.023942123434587e+15,
+  1.0239874029538475e+21
+]
+
+distances_to_stars.sum #=> 1.0473617207163307e+21
+distances_to_stars.average #=> 1.745602867860551e+20
+distances_to_stars.product #=> 7.233779610009546e+105
+
+class Array
+  def sum
+    reduce(&:+)
+  end
+
+  def average
+    sum/count
+  end
+
+  def product
+    reduce(&:*)
+  end
+end
+
+distances_to_stars.roughly.sum #=> 1e+21
+distances_to_stars.roughly.average #=> 1e+20
+distances_to_stars.roughly.product #=> 1e+206
+
+# solve for roughly
